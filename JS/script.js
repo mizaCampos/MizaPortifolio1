5254c6fff4af75f9e68165051f2524e92d93fc46
Mizael Lauriano <102499938+mizaCampos@users.noreply.github.com>
2022-05-03 13:55:13 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.conteudo', {
+    duration: 1000
+});
+
+sr.reveal('.sobre', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 2000
+})
+
+sr.reveal('.linguagens-blocos', {
+    duration: 3000
+})
+
